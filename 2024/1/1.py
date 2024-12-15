449ddb97ebca1e58de40b731c22052667003c877
dan <92388017+d1dotlol@users.noreply.github.com>
2024-12-15 20:29:41 +0000

    wtf

--- 2024/1/1.py
+++ 2024/1/1.py
@@ -1,6 +1,24 @@
-def main():
-    print("hi")
+import sys
+
+
+def main(file_name):
+    file_name = "2024/1/" + file_name
+    with open(file_name, "r") as file:
+        content = file.read()
+    content = content.split("\n")
+    total = 0
+    for line in content:
+        try:
+            line = line.split()
+            l = int(line[0])
+            r = int(line[-1])
+            total += abs(l-r)
+        except:
+            pass
+    print(total)
 
 
 if __name__ == '__main__':
-    main()
+    # Exclude script name
+    args = sys.argv[1:]
+    main(args[0])
